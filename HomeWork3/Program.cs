655a550f9ee9c1bccc04d35c7858654ef75e312b
Ogarev Albert <aealberttt@gmail.com>
2022-10-03 19:36:36 +0300

    Задача 3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sololearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            if (a % 2 == 0){
+                Console.WriteLine("Четное");
+            }
+            else{
+                  Console.WriteLine("Не четное");
+            }
+
+        }
+    }
+}
