03cddb06d103fc6359f290ec910e92b491639a96
Ogarev Albert <aealberttt@gmail.com>
2022-10-03 19:36:22 +0300

    Задача 1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа
+//  и выдаёт, какое число большее, а какое меньшее.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sololearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one_count = Convert.ToInt32(Console.ReadLine());
+            int two_count = Convert.ToInt32(Console.ReadLine());
+            int max = 0;
+            int min = 0;
+            
+            if(one_count > two_count){
+                 max = one_count;
+                 min = two_count;
+                 Console.WriteLine(max + " - more number");
+                 Console.WriteLine(min + " - lesser number");
+            }
+            else
+            {
+                 min = one_count;
+                 max = two_count;
+                 Console.WriteLine(max + " - more number");
+                 Console.WriteLine(min + " - lesser number");
+            }
+            
+        }
+    }
+}
