6a2c162f25245d4d7b7c84ead54b8b096e4f8f11
Ogarev Albert <aealberttt@gmail.com>
2022-10-03 19:36:30 +0300

    Задача 2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 2: Напишите программу, 
+// которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sololearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int max, min, mid;
+
+            if (a > b && a > c)
+            {
+                max = a;
+                min = (b < c) ? b : c;
+                Console.WriteLine(max);
+            }
+            else if (b > a && b > c)
+            {
+                max = b;
+                min = (a < c) ? a : c;
+                Console.WriteLine(max);
+            }
+            else
+            {
+                max = c;
+                min = (b < a) ? a : b;
+                Console.WriteLine(max);
+            }
+
+        }
+    }
+}
