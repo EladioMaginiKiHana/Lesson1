e7f35325b5fa88ba9edb6cac9cd44c267f5b835b
Ogarev Albert <aealberttt@gmail.com>
2022-10-18 20:59:25 +0300

    Homework_two

---
+++ Lesson4/Online/home/HomeWorkSecond/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+// Функция должна на вход принимать число, а выдавать сумму его цифр
+// 452-> 11
+// 82-> 10
+// 9012-> 12
+
+int sumNumbers(int value)
+{
+    if (value < 10 && value >= 0)
+        return value;
+    int digit = value % 10;
+    int nextValue = value / 10;
+    return digit + sumNumbers(nextValue);
+}
+
+Console.Write("Введите число, чтобы найти сумму его чисел: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр введеного вами числа: {sumNumbers(numbers)}");
