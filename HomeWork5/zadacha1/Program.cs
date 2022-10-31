c2f79e239634b0bb3f37a7785d785a105975db27
Ogarev Albert <aealberttt@gmail.com>
2022-10-31 09:24:06 +0300

    domashka_1

--- HomeWork5/zadacha1/Program.cs
+++ HomeWork5/zadacha1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a + b;
+
+try 
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int result = a + b;
+    
+    Console.WriteLine(result);
+}
---
+++ lesson7/domashka_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Random rnd = new Random();
+
+Console.Write("Введите количество рядков в двумерном массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в двумерном массиве: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = new double[rows, columns];
+
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        myArray[i, j] = rnd.Next(-100, 101) - Math.Round(rnd.NextDouble(), 1);
+        Console.Write($"{myArray[i, j]} ");
+    }
+    Console.WriteLine();
+}
